7ce1e8c56301c3c4ce235161808c2c0d30a47b97
rnaz25 <rnaz25@gmail.com>
2021-10-02 10:53:50 +0530

    query selectors added

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+const dateOfBirth = document.querySelector(".dob");
+const submitBtn = document.querySelector("#submit-button");
+const outputElement = document.querySelector("#output");
+
+
+
+
+
+
+submitBtn.addEventListener("click", )
